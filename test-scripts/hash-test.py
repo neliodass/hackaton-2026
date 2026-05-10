c8a687ec5485febda9cfff4b8f6f8da19e3e509b
psarnecki <piotr.sarnecki03@gmail.com>
2026-05-10 11:09:13 +0200

    Remove redundant markdown files and delete empty folders

--- test-scripts/hash-test.py
+++
@@ -1,6 +0,0 @@
-# w osobnym skrypcie lub python REPL
-from pathlib import Path
-p = Path("../server/packages/update.zip")
-data = bytearray(p.read_bytes())
-data[100] ^= 0xFF  # flip bajt na pozycji 100
-p.write_bytes(data)
